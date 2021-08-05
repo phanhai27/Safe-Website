3753bc35b54d49d3017292442f3fb99cbef59ad4
phanhai <phanhai27@gmail.com>
2021-08-05 14:47:11 +0700

    can change content of home page via json file

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import { getFontDefinitionFromNetwork } from 'next/dist/next-server/server/font-utils'
 import Head from 'next/head'
+import homeData from '../homepage.json'
 
 function IndexPage() {
     const styling = {
@@ -9,25 +11,25 @@ function IndexPage() {
         <Head>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <title>SafeWeb - Protect your project</title>
+            <title>{homeData.title}</title>
 
-            <meta name="description" content="Effective remote work collaboration" />
+            <meta name="description" content={homeData.meta.description} />
     
-            <meta itemprop="name" content="SafeWeb - Protect your project"/>
-            <meta itemprop="description" content="Effective remote work collaboration"/>
-            <meta itemprop="image" content="/static/img/safeweb-app-tracking.jpg"/>
+            <meta itemprop="name" content={homeData.title}/>
+            <meta itemprop="description" content={homeData.meta.description}/>
+            <meta itemprop="image" content={homeData.meta.image}/>
 
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://safeweb.app"/>
-            <meta property="og:title" content="SafeWeb - Protect your project"/>
-            <meta property="og:description" content="Effective remote work collaboration"/>
-            <meta property="og:image" content="/static/img/safeweb-app-tracking.jpg"/>
+            <meta property="og:url" content={homeData.meta.url}/>
+            <meta property="og:title" content={homeData.title}/>
+            <meta property="og:description" content={homeData.meta.description}/>
+            <meta property="og:image" content={homeData.meta.image}/>
 
             <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content="https://safeweb.app"/>
-            <meta property="twitter:title" content="SafeWeb - Protect your project"/>
-            <meta property="twitter:description" content="Effective remote work collaboration"/>
-            <meta property="twitter:image" content="/static/img/safeweb-app-tracking.jpg"/>
+            <meta property="twitter:url" content={homeData.meta.url}/>
+            <meta property="twitter:title" content={homeData.title}/>
+            <meta property="twitter:description" content={homeData.meta.description}/>
+            <meta property="twitter:image" content={homeData.meta.image}/>
             
             <link rel="icon" href="/static/img/favicon.png" sizes="32x32" type="image/png"/>
             <link rel="stylesheet" href="/static/css/custom.css"/>
@@ -56,12 +58,10 @@ function IndexPage() {
                 </div>
             </header>
             <h1 data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="display-3 text-white font-weight-bold my-5">
-            	A New Way<br/>
-            	To Work Remotely
+            	{homeData.header.part1}<br/>{homeData.header.part2}
             </h1>
             <p data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="lead text-white my-4">
-                Working in a virtual office environment, each person sitting in a remote place.
-                <br/> Not wasting time, everyone was highly focused.
+                {homeData.header.part3}<br/> {homeData.header.part4}
             </p>
             <a href="https://safeweb.app/download" target="_blank" data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="btn my-4 font-weight-bold atlas-cta cta-green">Get Started</a>
         </div>
@@ -73,22 +73,22 @@ function IndexPage() {
     `}</style>
 
     <div class="container my-5 py-2">
-        <h2 class="text-center font-weight-bold my-5">Highly focused on your project</h2>
+        <h2 class="text-center font-weight-bold my-5">{homeData.topic.head}</h2>
         <div class="row">
             <div data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center">
                 <img src="/static/img/smart-protect-1.jpg" alt="Anti-spam" class="mx-auto"/>
-                <h4>High efficiency work</h4>
-                <p>When you lose focus, the app will remind you.</p>
+                <h4>{homeData.topic.part1}</h4>
+                <p>{homeData.topic.message1}</p>
             </div>
             <div data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center">
                 <img src="/static/img/smart-protect-2.jpg" alt="Phishing Detect" class="mx-auto"/>
-                <h4>Avoid wasting time</h4>
-                <p>App assists you in getting rid of useless things.</p>
+                <h4>{homeData.topic.part2}</h4>
+                <p>{homeData.topic.message2}</p>
             </div>
             <div data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center">
                 <img src="/static/img/smart-protect-3.jpg" alt="Smart Scan" class="mx-auto"/>
-                <h4>Real-time reporting</h4>
-                <p>The App actively sends warnings to the leader.</p>
+                <h4>{homeData.topic.part3}</h4>
+                <p>{homeData.topic.message3}</p>
             </div>
         </div>
     </div>
@@ -97,9 +97,8 @@ function IndexPage() {
         <div class="container my-5">
             <div class="row justify-content-between text-center text-md-left">
                 <div data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="col-md-6">
-                    <h2 class="font-weight-bold">Take a look inside</h2>
-                    <p class="my-4">Team transparency in the project,
-                        <br/> details of the work that members are trying to develop.</p>
+                    <h2 class="font-weight-bold">{homeData.feature.part1.head}</h2>
+                    <p class="my-4">{homeData.feature.part1.message}<br/>{homeData.feature.part1.detail}</p>
                     <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-blue">Learn More</a>
                 </div>
                 <div data-aos="fade-left" data-aos-duration="1000" data-aos-once="true" class="col-md-6 align-self-center">
@@ -113,11 +112,8 @@ function IndexPage() {
         <div class="container">
             <div class="row justify-content-between text-center text-md-left">
                 <div data-aos="fade-left" data-aos-duration="1000" data-aos-once="true" class="col-md-6 flex-md-last">
-                    <h2 class="font-weight-bold">Safe and reliable</h2>
-                    <p class="my-4">
-                        App runs smoothly, does not affect your work.
-                        <br/> You can control multiple apps remotely at the same time.
-                    </p>
+                    <h2 class="font-weight-bold">{homeData.feature.part2.head}</h2>
+                    <p class="my-4">{homeData.feature.part2.message}<br/>{homeData.feature.part2.detail}</p>
                     <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-blue">Learn More</a>
                 </div>
                 <div data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="col-md-6 align-self-center flex-md-first">
@@ -131,37 +127,37 @@ function IndexPage() {
         <h2 class="text-center font-weight-bold d-block mb-3">Check our pricing</h2>
         <div class="row">
             <div data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center py-4 mt-5">
-                <h4 class="my-4">FREE</h4>
-                <p class="font-weight-bold">$ <span class="display-2 font-weight-bold">0</span> / MO.</p>
+                <h4 class="my-4">{homeData.payment.free.type}</h4>
+                <p class="font-weight-bold">$ <span class="display-2 font-weight-bold">{homeData.payment.free.price}</span> / MO.</p>
                 <ul class="list-unstyled">
-                    <li>Up to 30 Reports</li>
-                    <li>Up to 30 Real-time</li>
-                    <li>4 team Members</li>
-                    <li>Limited Support</li>
+                    <li>{homeData.payment.free.service1}</li>
+                    <li>{homeData.payment.free.service2}</li>
+                    <li>{homeData.payment.free.service3}</li>
+                    <li>{homeData.payment.free.service4}</li>
                 </ul>
-                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-ghost">Get Free</a>
+                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-ghost">{homeData.payment.free.button}</a>
             </div>
             <div data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center py-4 mt-5 rounded" id="price-table__premium">
-                <h4 class="my-4">STANDARD</h4>
-                <p class="font-weight-bold">$ <span class="display-2 font-weight-bold">7</span> / MO.</p>
+            <h4 class="my-4">{homeData.payment.standard.type}</h4>
+                <p class="font-weight-bold">$ <span class="display-2 font-weight-bold">{homeData.payment.standard.price}</span> / MO.</p>
                 <ul class="list-unstyled">
-                    <li>Up to 100 Reports</li>
-                    <li>Up to 100 Real-time</li>
-                    <li>30 team Members</li>
-                    <li>Limited Support</li>
+                    <li>{homeData.payment.standard.service1}</li>
+                    <li>{homeData.payment.standard.service2}</li>
+                    <li>{homeData.payment.standard.service3}</li>
+                    <li>{homeData.payment.standard.service4}</li>
                 </ul>
-                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-green">Buy Now</a>
+                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-green">{homeData.payment.standard.button}</a>
             </div>
             <div data-aos="fade-left" data-aos-delay="200" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center py-4 mt-5">
-                <h4 class="my-4">PREMIUM</h4>
-                <p class="font-weight-bold">$ <span class="display-2 font-weight-bold">14</span> / MO.</p>
+            <h4 class="my-4">{homeData.payment.premium.type}</h4>
+                <p class="font-weight-bold">$ <span class="display-2 font-weight-bold">{homeData.payment.premium.price}</span> / MO.</p>
                 <ul class="list-unstyled">
-                    <li>1000 Reports</li>
-                    <li>1000 Real-time</li>
-                    <li>100 Members</li>
-                    <li>Unlimited Support</li>
+                    <li>{homeData.payment.premium.service1}</li>
+                    <li>{homeData.payment.premium.service2}</li>
+                    <li>{homeData.payment.premium.service3}</li>
+                    <li>{homeData.payment.premium.service4}</li>
                 </ul>
-                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-ghost">Buy Now</a>
+                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-ghost">{homeData.payment.premium.button}</a>
             </div>
         </div>
     </div>
@@ -197,13 +193,12 @@ function IndexPage() {
                 <div class="col-md-6 text-white">
                     <h2 class="font-weight-bold">Contact Us</h2>
                     <p class="my-4">
-                        If you want to add features or buy an on-premise version,
-                        <br /> please contact the address below.
+                        {homeData.footer.part1}<br/>{homeData.footer.part2}
                     </p>
                     <ul class="list-unstyled">
-                        <li>Email : qa@safeweb.app</li>
-                        <li>Phone : +84-989-550-390</li>
-                        <li>Address : Hanoi, Vietnam</li>
+                        <li>Email : {homeData.footer.email}</li>
+                        <li>Phone : {homeData.footer.phone}</li>
+                        <li>Address : {homeData.footer.address}</li>
                     </ul>
                 </div>
                 <div class="col-md-6">
