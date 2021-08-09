241d7721ed254a14ed9a1898dbe496c0c7a83555
phanhai <phanhai27@gmail.com>
2021-08-09 16:34:43 +0700

    jquery change html tag <a>

--- public/static/js/checkversion.js
+++ public/static/js/checkversion.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	let root = 'https://dev-test.safeweb.app';
+	let root = 'https://safeweb.app';
 	let urlAPI = root + '/api/v1/version/list';
     $.getJSON(urlAPI, function(data) {
 		$.each(data.list, function(key,value) {
 			if (value.platform == `windows`) {
-				$('#dlink').attr("href", `${root}/download/SafeApp.v${value.version}.msi`);
-				$("#dlink").text(`Windows version "${value.version}`);
+				$('#dlink').attr("href", `${root}/download/SafeApp_v${value.version}.msi`);
+				$("#dlink small").text(`Windows version ${value.version}`);
 			}
 		  });
     });
