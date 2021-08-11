c943a50c7f3f380461f9bc23d473053bbd7b8d02
phanhai <phanhai27@gmail.com>
2021-08-11 21:34:32 +0700

    add language menu and logo in blog page and posts

--- src/components/blog-header.js
+++ src/components/blog-header.js
@@ -1,8 +1,11 @@
+import LanguageMenu from './LanguageMenu';
+
 export default function BlogHeader ({lang}) {
     return (
             <header id="header">
-              <h1><a href={"/" + lang}>Home</a></h1>
+              <h1><a href={"/" + lang}><img src="/static/img/logo.png" alt="logo" width="30" height="30"/></a></h1>
               <h1><a href={"/" + lang + "/blog"}>SafeWeb Stories</a></h1>
+              <LanguageMenu/>
             </header>
     )
 }
--- src/components/blog-intro.js
+++ src/components/blog-intro.js
@@ -1,7 +1,7 @@
 export default function BlogIntro () {
     return (
       <section id="intro">
-						<a href="/"><img src="/static/img/logo.png" alt="" width="60" height="60"/></a>
+						{/* <a href="/"><img src="/static/img/logo.png" alt="" width="60" height="60"/></a> */}
 						<header>
 							<h2>The future of working remotely</h2>
 							<p>One day, robots will work for us so that we have more time for life.</p>
