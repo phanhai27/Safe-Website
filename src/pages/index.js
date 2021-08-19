8fc31ceff6d77690efe2d9b9392a8a26e7d60842
phanhai <phanhai27@gmail.com>
2021-08-19 12:01:48 +0700

    auto detect location of client

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,32 @@
-import { useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
-
 import i18next from 'i18next';
 
 export default function Home() {
 	const router = useRouter();
 
-	useEffect(() => {
+	const detectCountry=()=>{
+		fetch('http://ip-api.com/json'
+		,{
+		  headers : { 
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		   }
+		}
+		)
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(myJson) {
+			if (myJson.countryCode = 'VN') {
+				i18next.changeLanguage('vi');
+			}
+		});
+	};
+
+	React.useEffect(() => {
+		detectCountry();
+		
 		const { pathname } = router;
 		if (pathname == '/') {
 			router.push('/' + i18next.language.substring(0, 2));
