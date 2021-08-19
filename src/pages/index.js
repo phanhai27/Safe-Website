a7b61338e33f342b539c1d360bc4ee9df9eb0b6d
phanhai <phanhai27@gmail.com>
2021-08-19 15:12:50 +0700

    fix detect country: add async await

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,32 +5,19 @@ import i18next from 'i18next';
 export default function Home() {
 	const router = useRouter();
 
-	const detectCountry=()=>{
-		fetch('http://ip-api.com/json'
-		,{
-		  headers : { 
-			'Content-Type': 'application/json',
-			'Accept': 'application/json'
-		   }
-		}
-		)
-		.then(function(response){
-			return response.json();
-		})
-		.then(function(myJson) {
-			if (myJson.countryCode = 'VN') {
-				i18next.changeLanguage('vi');
-			}
-		});
-	};
-
 	React.useEffect(() => {
-		detectCountry();
-		
-		const { pathname } = router;
-		if (pathname == '/') {
-			router.push('/' + i18next.language.substring(0, 2));
-		}
+		(async () => {
+			const response = await fetch('http://ip-api.com/json');
+  			const myJson = await response.json();
+			const { pathname } = router;
+			const viLang = 'vi';
+			if (pathname == '/' && myJson.countryCode == 'VN') {
+				i18next.changeLanguage(viLang);
+				router.push('/' + viLang);
+			} else {
+				router.push('/' + i18next.language.substring(0, 2));
+			}
+		})();
 	});
 
 	return null;
