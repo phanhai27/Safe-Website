764214c88279947d496583d017da194bce502c0b
phanhai <phanhai27@gmail.com>
2021-08-21 10:23:03 +0700

    update name of installer

--- public/static/js/checkversion.js
+++ public/static/js/checkversion.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	let urlAPI = 'https://safeweb.app/api/v1/version/list';
+	let pathName = window.location.pathname;
+	let subPath = pathName.split('/');
+	let lang = subPath[1];
+
     $.getJSON(urlAPI, function(data) {
 		$.each(data.list, function(key,value) {
 			if (value.platform == `windows`) {
-				$('#dlink').attr("href", `/download/SafeAppSetup_v${value.version}.exe`);
+				$('#dlink').attr("href", `/download/SafeApp-v${value.version}.msi`);
 				$("#dlink small").text(`Windows version ${value.version}`);
 			}
 		  });
