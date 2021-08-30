28204a347665c26fb35fd62e2b8c0136ad135e22
phanhai <phanhai27@gmail.com>
2021-08-30 17:27:18 +0700

    update content for article kid protection

--- src/components/blog-head.js
+++ src/components/blog-head.js
@@ -9,22 +9,22 @@ export default function BlogHead ({postData, lang}) {
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-            <title>{postData.article.title} - SafeWeb</title>
+            <title>{postData.article.title + "- SafeWeb"}</title>
             <meta name="description" content={postData.article.description}/>
 
-            <meta itemProp="name" content={postData.article.title}/>
+            <meta itemProp="name" content={postData.article.title + "- SafeWeb"}/>
             <meta itemProp="description" content= {postData.article.description} />
             <meta itemProp="image" content={ENV.BASE_URL + postData.article.picture}/>
 
             <meta property="og:type" content="website"/>
             <meta property="og:url" content={ENV.BASE_URL + "/" + lang + "/" + postData.article.url}/>
-            <meta property="og:title" content={postData.article.title}/>
+            <meta property="og:title" content={postData.article.title + "- SafeWeb"}/>
             <meta property="og:description" content={postData.article.description}/>
             <meta property="og:image" content={ENV.BASE_URL + postData.article.picture}/>
 
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content={ENV.BASE_URL + "/" + lang + "/" + postData.article.url}/>
-            <meta property="twitter:title" content={postData.article.title}/>
+            <meta property="twitter:title" content={postData.article.title + "- SafeWeb"}/>
             <meta property="twitter:description" content={postData.article.description}/>
             <meta property="twitter:image" content={ENV.BASE_URL + postData.article.picture}/>
 
