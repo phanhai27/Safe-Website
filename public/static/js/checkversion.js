7f33a249499167e46dcd7791a212bc8ec2eae36c
phanhai <phanhai27@gmail.com>
2021-10-14 21:46:23 +0700

    update js funcion to get version

--- public/static/js/checkversion.js
+++ public/static/js/checkversion.js
@@ -3,12 +3,27 @@ $(document).ready(function(){
 	let pathName = window.location.pathname;
 	let subPath = pathName.split('/');
 	let lang = subPath[1];
+	let verLang = 'Version';
+	switch(lang) {
+		case 'vi':
+		  verLang = 'Phiên bản';
+		  break;
+		case 'zh':
+		  verLang = '版本';
+		  break;
+		case 'ja':
+		  verLang = 'バージョン';
+		  break;
+		default:
+		  verLang = 'Version';
+	}
 
     $.getJSON(urlAPI, function(data) {
 		$.each(data.list, function(key,value) {
+			
 			if (value.platform == `windows`) {
 				$('#dlink').attr("href", `/download/SafeApp-v${value.version}-setup.exe`);
-				$("#dlink small").text(`Version ${value.version}`);
+				$("#dlink small").text(`${verLang} ${value.version}`);
 			}
 		  });
     });
