27b1fcf6492f8269c0173df203b33733efade9b2
phanhai <phanhai27@gmail.com>
2022-02-24 15:01:42 +0700

    js update price wrong when change language

--- public/static/js/price.js
+++ public/static/js/price.js
@@ -3,20 +3,21 @@ const standard = document.getElementById("standard-price");
 const preminum = document.getElementById("preminum-price");
 
 checkbox.addEventListener("click", () => {
-  if (standard.textContent == "44.000đ") {
-    standard.textContent = "440.000đ"
-  } else if (standard.textContent == "440.000đ") {
-    standard.textContent = "44.000đ"
-  } else {
-    standard.textContent = standard.textContent === "$20" ? "$2" : "$20";
+  if (checkbox.checked) { // yearly
+    if (standard.textContent.includes("$")){  // international price
+      standard.textContent = "$20";
+      preminum.textContent = "$66";
+    } else {
+      standard.textContent = "440.000đ";
+      preminum.textContent = "1.452.000đ";
+    }
+  } else {  // monthly
+    if (standard.textContent.includes("$")){  // international price
+      standard.textContent = "$2";
+      preminum.textContent = "$6";
+    } else {
+      standard.textContent = "44.000đ";
+      preminum.textContent = "132.000đ";
+    }
   }
-
-  if (preminum.textContent == "132.000đ") {
-    preminum.textContent = "1.452.000đ"
-  } else if (preminum.textContent == "1.452.000đ") {
-    preminum.textContent = "132.000đ"
-  } else {
-    preminum.textContent = preminum.textContent === "$66" ? "$6" : "$66";
-  }
-
 });
